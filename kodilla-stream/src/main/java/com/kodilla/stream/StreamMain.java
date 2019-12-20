e3cdfe17103ff5d836d69c8a3374d7c4244352d0
DamianPaziewski <nasz_email_do_github>
2019-12-20 22:08:59 +0000

    Immutable CLass

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class StreamMain {
         beautifierExecutor.beautify("Caution do not enter", (txt) -> txt.toUpperCase());
         beautifierExecutor.beautify("Super", (txt) -> txt+" "+(txt.replace('S','D')));
         beautifierExecutor.beautify("My name is", (txt) -> txt+"...");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
