3cef60c95a1c176ffc535f50ab77ca0469d277e1
DamianPaziewski <nasz_email_do_github>
2019-12-21 17:38:40 +0000

    Forum Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,9 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,7 +13,7 @@ public class StreamMain {
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMapOfUsers = theForum.getUserList().stream()
                 .filter(user -> user.getUserSex() == 'M')
-                .filter(user -> user.getUserDateOfBirth().getYear() <= 1999)
+                .filter(user -> Period.between(user.getUserDateOfBirth(), LocalDate.now()).getYears() >= 20)
                 .filter(user -> user.getUserPostsQty() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserIdNo, user -> user));
 
