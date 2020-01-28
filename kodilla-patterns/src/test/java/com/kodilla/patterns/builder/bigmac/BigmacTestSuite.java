46d208a9915d7f56e2375fcf62f60c457bd6761e
DamianPaziewski <nasz_email_do_github>
2020-01-28 22:23:39 +0000

    Builder

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import com.kodilla.patterns.builder.pizza.Pizza;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredients(IngredientsType.ONION)
+                .burgers(2)
+                .sauce(SauceType.THOUSAND_ISLANDS)
+                .ingredients(IngredientsType.CHEESE)
+                .bun(BunType.STANDARD)
+                .ingredients(IngredientsType.CUCUMBER)
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        int howManyBurgers = bigmac.getBurgers();
+        BunType whichBun = bigmac.getBun();
+        SauceType whichSauce = bigmac.getSauce();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+        Assert.assertEquals(2, howManyBurgers);
+        Assert.assertEquals(BunType.STANDARD, whichBun);
+        Assert.assertEquals(SauceType.THOUSAND_ISLANDS, whichSauce);
+    }
+}
